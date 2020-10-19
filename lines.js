aefc26870d815728d6c81bd016f89a4a2b0f7101
Manav Misra <manavm@visionwebsoft.com>
2020-10-18 19:55:03 -0500

    :sparkles: Add 'lines' solution

---
+++ lines.js
@@ -0,0 +1,14 @@
+const { Writable } = require("stream");
+
+const split2 = require("split2");
+
+// const lineCounter = 0;
+
+const writable = new Writable({
+  write(line, _, next) {
+    console.log(line.toString().toLowerCase());
+    next();
+  },
+});
+
+process.stdin.pipe(split2()).pipe(writable);
