3902767f5ecfa1b6cd90e9842462974df09ea9ca
Manav Misra <manavm@visionwebsoft.com>
2020-10-19 17:22:31 -0500

    :sparkles: Add solution for 'duplexer-redux'

--- duplexer-redux.js
+++ duplexer-redux.js
@@ -1,14 +1,26 @@
+const { count } = require("console");
 const duplexer = require("duplexer3");
-const { Transform } = require("stream");
+const { finished, Transform } = require("stream");
 
-const transformer = new Transform({
-  writableObjectMode: true,
-  transform: (data, _, next) => {
-    console.log(data);
+const countryCount = {};
+
+const transform = new Transform({
+  objectMode: true,
+  transform({ country }, _, next) {
+    countryCount[country] = (countryCount[country] || 0) + 1;
+    this.push(countryCount);
     next();
   },
 });
 
 module.exports = (counter) => {
-  return duplexer({ objectMode: true }, transformer, counter);
+  const duplex = duplexer({ objectMode: true }, transform, counter);
+
+  finished(transform, () => {
+    counter.setCounts(countryCount);
+  });
+
+  transform.resume();
+
+  return duplex;
 };
