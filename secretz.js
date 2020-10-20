36fa5357243aa2474171f2feebf260fcbdb54a99
Manav Misra <manavm@visionwebsoft.com>
2020-10-20 15:37:49 -0500

    :construction: Log file info on each 'entry' event

---
+++ secretz.js
@@ -0,0 +1,31 @@
+const crypto = require("crypto");
+
+const concat = require("concat-stream");
+const tar = require("tar");
+
+const [_, __, algo, key, iv] = process.argv;
+
+const tarParser = new tar.Parse();
+
+tarParser.on(
+  "entry",
+  // Event wistener receives a readable stream of file contents
+  (ev) => {
+    // If this is not a file, nothing to see here, move along!
+    if (ev.type !== "File") {
+      ev.resume();
+    }
+
+    // for each file, create an encoded hash
+    const hashStream = crypto.createHash("md5", { encoding: "hex" });
+    console.log(ev.path);
+
+    ev.resume();
+  }
+);
+
+/**
+ * Decrypt file.
+ * Unzip it.
+ */
+process.stdin.pipe(crypto.createDecipheriv(algo, key, iv)).pipe(tarParser);
