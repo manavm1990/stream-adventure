4df69e2a1b260c258e410caae59fc5be3afadafb
Manav Misra <manavm@visionwebsoft.com>
2020-10-20 07:33:10 -0500

    :construction: Log 'categorizedBooks' JSON-ified

--- combiner.js
+++ combiner.js
@@ -2,10 +2,20 @@ const combine = require("stream-combiner2");
 const split2 = require("split2");
 const { Transform } = require("stream");
 
+const categorizedBooks = {};
+
 const transform = new Transform({
   writableObjectMode: true,
   transform(data, _, next) {
-    console.log(data);
+    const json = JSON.parse(data);
+    if (json.type === "genre") {
+      categorizedBooks.name = json.name;
+    } else if (categorizedBooks.books) {
+      categorizedBooks.books.push(json.name);
+    } else {
+      categorizedBooks.books = [];
+    }
+    console.log(JSON.stringify(categorizedBooks));
     next();
   },
 });
